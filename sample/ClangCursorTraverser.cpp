73600c390494669cfe01fff63f5da6b1a978f87f
ousttrue <ousttrue@gmail.com>
2020-04-19 10:28:43 +0900

    TraverseChildren

--- sample/ClangCursorTraverser.cpp
+++ sample/ClangCursorTraverser.cpp
@@ -1,82 +1,221 @@
 #include "ClangCursorTraverser.h"
-#include "enum_name.h"
+
 #include <clang-c/Index.h>
-#include <functional>
 #include <plog/Log.h>
+
+#include <functional>
+
+#include "enum_name.h"
 
 using CallbackFunc = std::function<void(const CXCursor &)>;
 
-static CXChildVisitResult visitor(CXCursor cursor, CXCursor parent,
-                                  void *data) {
-  (*reinterpret_cast<CallbackFunc *>(data))(cursor);
-  return CXChildVisit_Continue;
+static CXChildVisitResult visitor(CXCursor cursor, CXCursor parent, void *data)
+{
+    (*reinterpret_cast<CallbackFunc *>(data))(cursor);
+    return CXChildVisit_Continue;
 };
-static void processChildren(const CXCursor &cursor,
-                            const CallbackFunc &callback) {
-  clang_visitChildren(cursor, &visitor, (void *)&callback);
+static void processChildren(const CXCursor &cursor, const CallbackFunc &callback)
+{
+    clang_visitChildren(cursor, &visitor, (void *)&callback);
 }
 
-class ScopedCXString {
-  const CXString &m_str;
-  const char *m_c_str;
+class ScopedCXString
+{
+    const CXString &m_str;
+    const char *m_c_str;
 
-  ScopedCXString(const ScopedCXString &) = delete;
-  ScopedCXString &operator=(const ScopedCXString &) = delete;
+    ScopedCXString(const ScopedCXString &) = delete;
+    ScopedCXString &operator=(const ScopedCXString &) = delete;
 
-public:
-  ScopedCXString(const CXString &str) : m_str(str) {
-    m_c_str = clang_getCString(str);
-  }
-  ~ScopedCXString() { clang_disposeString(m_str); }
-  const char *c_str() const { return m_c_str; }
-};
-
-class TraverserImpl {
-public:
-  void Traverse(const CXCursor &cursor) {
-
-    ScopedCXString cursorSpelling(clang_getCursorSpelling(cursor));
-
-    auto location = clang_getCursorLocation(cursor);
-    if (clang_equalLocations(location, clang_getNullLocation())) {
-      // no location
-      LOGD << " \"" << cursorSpelling.c_str() << "\" " << (int)cursor.kind
-           << " => "
-           << enum_name_map<CXCursorKind, (int)CXCursor_OverloadCandidate>::get(
-                  cursor.kind);
-
-    } else {
-
-      CXFile file;
-      uint32_t line;
-      uint32_t column;
-      uint32_t offset;
-      clang_getInstantiationLocation(location, &file, &line, &column, &offset);
-      if (!file) {
-        // no file
-        LOGD << "no file";
-      } else {
-        ScopedCXString fileStr(clang_getFileName(file));
-
-        LOGD
-            << fileStr.c_str() << ":" << line << ":"
-            << column // << ":" << offset
-            << " \"" << cursorSpelling.c_str() << "\" " << (int)cursor.kind
-            << " => "
-            << enum_name_map<CXCursorKind,
-                             (int)CXCursor_OverloadCandidate>::get(cursor.kind);
-      }
+  public:
+    ScopedCXString(const CXString &str) : m_str(str)
+    {
+        m_c_str = clang_getCString(str);
     }
-  }
+    ~ScopedCXString()
+    {
+        clang_disposeString(m_str);
+    }
+    const char *c_str() const
+    {
+        return m_c_str;
+    }
+    std::string_view str_view() const
+    {
+        return m_c_str;
+    }
 };
 
-void ClangCursorTraverser::Traverse(const CXCursor &cursor) {
+class ScopedCXTokens
+{
+    CXTranslationUnit m_tu;
+    CXToken *m_tokens;
+    uint32_t m_count;
 
-  TraverserImpl impl;
+    ScopedCXTokens(const ScopedCXTokens &) = delete;
+    ScopedCXTokens &operator=(const ScopedCXTokens &) = delete;
 
-  CallbackFunc callback = [&impl](const CXCursor &child) {
-    impl.Traverse(child);
-  };
+  public:
+    ScopedCXTokens(const CXCursor &cursor)
+    {
+        auto range = clang_getCursorExtent(cursor);
+        m_tu = clang_Cursor_getTranslationUnit(cursor);
+        clang_tokenize(m_tu, range, &m_tokens, &m_count);
+    }
+    ~ScopedCXTokens()
+    {
+        clang_disposeTokens(m_tu, m_tokens, m_count);
+    }
+    uint32_t size() const
+    {
+        return m_count;
+    }
 
-  processChildren(cursor, callback);
+    ScopedCXString Spelling(uint32_t index) const
+    {
+        return ScopedCXString(clang_getTokenSpelling(m_tu, m_tokens[index]));
+    }
+};
+
+class TraverserImpl
+{
+  public:
+    void TraverseChildren(const CXCursor &cursor)
+    {
+        processChildren(cursor, std::bind(&TraverserImpl::Traverse, this, std::placeholders::_1));
+    }
+
+  private:
+    void Traverse(const CXCursor &cursor)
+    {
+        ScopedCXString spelling(clang_getCursorSpelling(cursor));
+
+        auto location = clang_getCursorLocation(cursor);
+        CXFile file = nullptr;
+        uint32_t line;
+        uint32_t column;
+        uint32_t offset;
+        if (!clang_equalLocations(location, clang_getNullLocation()))
+        {
+            clang_getInstantiationLocation(location, &file, &line, &column, &offset);
+        }
+
+        if (file)
+        {
+            ScopedCXString fileStr(clang_getFileName(file));
+            LOGD << fileStr.c_str() << ":" << line << ":" << column << ' ' // << ":" << offset
+                 << enum_name_map<CXCursorKind, (int)CXCursor_OverloadCandidate>::get(cursor.kind) << '('
+                 << (int)cursor.kind << ')' << " \"" << spelling.c_str() << "\" ";
+        }
+        else
+        {
+            LOGD << enum_name_map<CXCursorKind, (int)CXCursor_OverloadCandidate>::get(cursor.kind) << '('
+                 << (int)cursor.kind << ')' << " \"" << spelling.c_str() << "\" ";
+        }
+
+        switch (cursor.kind)
+        {
+        case CXCursor_InclusionDirective:
+        case CXCursor_ClassTemplate:
+        case CXCursor_ClassTemplatePartialSpecialization:
+        case CXCursor_FunctionTemplate:
+        case CXCursor_UsingDeclaration:
+        case CXCursor_StaticAssert:
+            // skip
+            break;
+
+        case CXCursor_MacroDefinition:
+            // parseMacroDefinition(cursor);
+            break;
+
+        case CXCursor_MacroExpansion:
+            if (spelling.str_view() == "DEFINE_GUID")
+            {
+                //   auto tokens = getTokens(cursor);
+                //   scope(exit)
+                //       clang_disposeTokens(tu, tokens.ptr, cast(uint)
+                //       tokens.length);
+                //   string[] tokenSpellings =
+                //       tokens.map !(t = > tokenToString(cursor,
+                //       t)).array();
+                //   if (tokens.length == 26) {
+                //     auto name = tokenSpellings[2];
+                //     if (name.startsWith("IID_")) {
+                //       name = name[4.. $];
+                //     }
+                //     m_uuidMap[name] = tokensToUUID(tokenSpellings[4..
+                //     $]);
+                //   } else {
+                //     debug auto a = 0;
+                //   }
+            }
+            break;
+
+        case CXCursor_Namespace:
+            //   {
+            //     context = context.enterNamespace(spelling);
+            //     foreach (child; cursor.getChildren()) {
+            //       traverse(child, context.getChild());
+            //     }
+            //   }
+            break;
+
+        case CXCursor_UnexposedDecl: {
+            ScopedCXTokens tokens(cursor);
+
+            if (tokens.size() >= 2)
+            {
+                // extern C
+                if (tokens.Spelling(0).str_view() == "extern" && tokens.Spelling(1).str_view() == "\"C\"")
+                {
+                    // context.isExternC = true;
+                }
+            }
+
+            TraverseChildren(cursor);
+        }
+        break;
+
+        case CXCursor_TypedefDecl:
+            // parseTypedef(cursor);
+            break;
+
+        case CXCursor_FunctionDecl:
+            //   {
+            //     auto decl =
+            //         parseFunction(cursor, &context,
+            //         clang_getCursorResultType(cursor));
+            //     if (decl) {
+            //       auto header = getOrCreateHeader(cursor);
+            //       header.types ~ = decl;
+            //     }
+            //   }
+            break;
+
+        case CXCursor_StructDecl:
+        case CXCursor_ClassDecl:
+            // parseStruct(cursor, context.enterNamespace(spelling), false);
+            break;
+
+        case CXCursor_UnionDecl:
+            // parseStruct(cursor, context.enterNamespace(spelling), true);
+            break;
+
+        case CXCursor_EnumDecl:
+            // parseEnum(cursor);
+            break;
+
+        case CXCursor_VarDecl:
+            break;
+
+        default:
+            throw std::runtime_error("unknown CXCursorKind");
+        }
+    }
+};
+
+void ClangCursorTraverser::Traverse(const CXCursor &cursor)
+{
+    TraverserImpl impl;
+    impl.TraverseChildren(cursor);
 }
--- sample/main.cpp
+++ sample/main.cpp
@@ -1,20 +1,42 @@
-#include "ClangIndex.h"
 #include <plog/Appenders/ConsoleAppender.h>
 #include <plog/Log.h>
+
 #include <string>
 #include <vector>
 
+#include "ClangIndex.h"
 
-int main(int argc, char **argv) {
-  // setup logger
-  static plog::ConsoleAppender<plog::TxtFormatter> consoleAppender;
-  plog::init(plog::verbose, &consoleAppender);
+namespace plog
+{
+class MyFormatter
+{
+  public:
+    static util::nstring header() // This method returns a header for a new file. In our case it is empty.
+    {
+        return util::nstring();
+    }
 
-  ClangIndex index;
-  if (!index.Parse("C:/Program Files/LLVM/include/clang-c/Index.h",
-                   "C:/Program Files/LLVM/include")) {
-    return 1;
-  }
+    static util::nstring format(const Record &record) // This method returns a string from a record.
+    {
+        util::nostringstream ss;
+        ss << record.getMessage() << "\n"; // Produce a simple string with a log message.
 
-  return 0;
+        return ss.str();
+    }
+};
+} // namespace plog
+
+int main(int argc, char **argv)
+{
+    // setup logger
+    static plog::ConsoleAppender<plog::MyFormatter> consoleAppender;
+    plog::init(plog::verbose, &consoleAppender);
+
+    ClangIndex index;
+    if (!index.Parse("C:/Program Files/LLVM/include/clang-c/Index.h", "C:/Program Files/LLVM/include"))
+    {
+        return 1;
+    }
+
+    return 0;
 }
