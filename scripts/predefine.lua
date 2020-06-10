3c39e3fac041705c4412e3d8bd77f30f1fae083b
ousttrue <ousttrue@gmail.com>
2020-06-10 13:03:43 +0900

    remote debug by LRDB

--- scripts/predefine.lua
+++ scripts/predefine.lua
@@ -1,5 +1,8 @@
 clalua = require "clalua"
 
+lrdb = require("lrdb_server")
+lrdb.activate(21110) --21110 is using port number. waiting for connection by debug client.
+
 function printf(fmt, ...)
     print(string.format(fmt, ...))
 end
